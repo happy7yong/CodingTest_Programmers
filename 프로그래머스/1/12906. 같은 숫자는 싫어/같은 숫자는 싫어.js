64221b77d1df4e880f5c24b52468ce377f9042bc
chaeyoung <happy7yong83@gmail.com>
2025-01-16 14:11:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 57.17 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr)
+{
+    var answer = [];
+    var answerRep = 0;
+    for(let i=0;i<arr.length;i++){
+                if((arr[i]!=answer[answer.length-1]) || (arr.length==0)){
+                    answer.push(arr[i]);
+                }
+    }
+    
+    return answer;
+}
