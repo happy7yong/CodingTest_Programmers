0c2f28e4f7324425aa4350b496598b2a0a8048b3
chaeyoung <happy7yong83@gmail.com>
2025-03-18 16:05:46 +0900

    [level 0] Title: 특이한 정렬, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,13 +1,11 @@
 function solution(numlist, n) {
-    // 내림차순 정렬
-    numlist.sort((a, b) => b - a);
+    numlist.sort((a,b)=>b-a)
     
-    // 차이와 절댓값 계산을 한 번에 처리
-    let numlist_index = numlist.map(num => ({ num, diff: Math.abs(num - n) }));
+    let numlist_diff = numlist.map(num=>({
+        num:num,
+        diff:Math.abs(num-n)
+    }))
     
-    // 차이에 따라 정렬
-    numlist_index.sort((a, b) => a.diff - b.diff);
-    
-    // 정렬된 숫자만 추출
-    return numlist_index.map(item => item.num);
+    numlist_diff.sort((a,b)=>a.diff-b.diff)
+    return numlist_diff.map(item=>item.num)
 }
