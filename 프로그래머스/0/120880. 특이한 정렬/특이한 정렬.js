8571ca64e0a32161df7e481d16a9e8c1a22c732c
chaeyoung <happy7yong83@gmail.com>
2025-03-18 15:20:25 +0900

    [level 0] Title: 특이한 정렬, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,16 +1,13 @@
 function solution(numlist, n) {
-    numlist.sort((a,b)=>b-a)
+    // 내림차순 정렬
+    numlist.sort((a, b) => b - a);
     
-    var num_dif = numlist.map(num=>num-n);
-    var num_abs = num_dif.map(num=>Math.abs(num))
-    var numlist_index = [];
+    // 차이와 절댓값 계산을 한 번에 처리
+    let numlist_index = numlist.map(num => ({ num, diff: Math.abs(num - n) }));
     
-    for(let i=0;i<numlist.length;i++){
-        let num_min = Math.min(...num_abs)
-        numlist_index.push(numlist[num_abs.indexOf(num_min)])
-        num_abs[num_abs.indexOf(num_min)]=Infinity
-    }
-
-    return numlist_index;
+    // 차이에 따라 정렬
+    numlist_index.sort((a, b) => a.diff - b.diff);
     
+    // 정렬된 숫자만 추출
+    return numlist_index.map(item => item.num);
 }
