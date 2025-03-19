c3dc3ee4fb0e881f83ff1607475b988a9883b072
chaeyoung <happy7yong83@gmail.com>
2025-03-19 15:02:20 +0900

    [level 0] Title: A로 B 만들기, Time: 0.47 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,23 +1,3 @@
-const Counter = (Spell) => {
-    var Spell_arr = [...Spell];
-    var Spell_set = [...new Set(Spell_arr)]
-    
-    var Counter_arr = Spell_set.map(num=>({
-        speill : num,
-        count : Spell_arr.filter(before_item=>before_item==num).length
-    }))
-    
-    return Counter_arr
-}
-
 function solution(before, after) {
-    
-    let count_before = Counter(before)
-    let count_after = Counter(after)
-    
-    console.log(count_before, count_after)
-    var answer = count_before.map(num=>count_after.map((item=>num.speill==item.speill && num.count==item.count? 1:0)))
-    var count_answer = answer.map(num=>num.filter(item=>item==1).length==0?0:1)
-
-    return count_answer.length == count_answer.filter(num=>num==1).length?1:0;
+    return [...before].sort().join("")==[...after].sort().join("")?1:0;
 }
