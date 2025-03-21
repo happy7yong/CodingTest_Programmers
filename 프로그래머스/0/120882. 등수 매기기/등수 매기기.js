2acf13c14c3f2b65f9d688120ecc218e788e2ea3
chaeyoung <happy7yong83@gmail.com>
2025-03-21 15:49:06 +0900

    [level 0] Title: 등수 매기기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,31 +1,5 @@
 function solution(score) {
-    var score_order=[];
-    let score_answer = [];
-    
-    for(let i=0;i<score.length;i++){
-        score_order[i] = score[i].reduce((acc,cur)=>acc+cur)
-    }
-    
-    let score_sum = score_order.slice();
-    score_order.sort((a,b)=>b-a)
-
-    
-    for(let i=0;i<score_order.length;i++){
-        for(let j=0;j<score_sum.length;j++){
-            if(score_order[i] == score_sum[j]){
-                let index = i+1
-                if(score_order[i]==score_order[i-1] && i>0){
-                   continue;
-                }
-                score_answer[j] = index
-            }
-        }
-    }
-    console.log("score_order : "+score_order);
-    console.log("score_sum : "+score_sum);
-    
-    
-    
-    console.log(score_answer)
-    return score_answer;
+    let score_sum = score.map(num=>num[0]+num[1])
+    let score_sort = [...score_sum].sort((a,b)=>b-a)
+    return score_sum.map(num=> score_sort.indexOf(num)+1 );
 }
