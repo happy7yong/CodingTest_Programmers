ef982e6f2a6bbcab657ce2733e2e8e9cb85d8749
chaeyoung <happy7yong83@gmail.com>
2025-03-26 18:07:49 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.30 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 71.5 MB, 시간: 74.29 ms
+메모리: 71.7 MB, 시간: 67.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 24일 17:55:03
+2025년 03월 26일 18:07:42
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,22 +1,15 @@
 function solution(participant, completion) {
-    var map = {};
+    var map = {}; //해쉬
     
-    //0. 해쉬맵 초기화
-    for(let player of participant){
-        map[player]=0
+    for (let player of participant){
+        map[player] = (map[player] || 0) + 1
     }
     
-    //1. 해쉬맵에 저장
-    for(let player of participant){
-        map[player]+=1
-    }
-    //2. 해쉬맵에서 제거
     for(let player of completion){
         map[player]--
     }
-    //3. 남아있는 사람
+    
     for(let player in map){
-        if(map[player]>0)
-            return player
+        if(map[player] > 0) return player;
     }
 }
