492fc7f6a573f6e0b1202e8f11a114426ce101ba
chaeyoung <happy7yong83@gmail.com>
2025-04-07 11:36:17 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -1,10 +1,8 @@
 function solution(numbers, n) {
-    var answer = 0;
-    for(let i=0;i<numbers.length;i++){
-        if(answer > n){
-            break;
-        }
-        answer += numbers[i]
-    }
-    return answer;
+    var sum = 0;
+    numbers.some(num=>{
+        sum += num
+        return sum > n //some 메서드를 통해 조건에 해당할 경우 return 으로 for문 나감.
+    })
+    return sum;
 }
