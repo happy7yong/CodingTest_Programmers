e4f481cde251bd3bb356c846a447b14356a773f1
chaeyoung <happy7yong83@gmail.com>
2025-06-02 14:57:54 +0900

    [level 0] Title: k의 개수, Time: 29.03 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -1,14 +1,11 @@
 function solution(i, j, k) {
-    var count = 0;
-    var k_str = k.toString()
-    for(let a=i;a<=j;a++){
-        let a_clo= [...a.toString()];
-        for(const ele of a_clo){
-            if(ele.includes(k_str)){
-            
-                count++;
-            }      
-        }
+    var answer = 0;
+    let k_str = String(k);
+    
+    for(let n=i;n<=j;n++){
+        answer += [...String(n)].filter(_=>_==k).length
     }
-    return count;
+    
+    
+    return answer;
 }
