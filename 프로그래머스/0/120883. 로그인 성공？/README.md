be14b9e97168e790eef0d15efc4b31979f156eab
chaeyoung <happy7yong83@gmail.com>
2025-06-02 15:04:19 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/README.md
+++ 프로그래머스/0/120883. 로그인 성공？/README.md
@@ -1,10 +1,10 @@
 # [level 0] 로그인 성공? - 120883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120883#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120883) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 4.72 ms
+메모리: 33.5 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 15:37:33
+2025년 06월 02일 15:04:17
 
 ### 문제 설명
 
--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,27 +1,7 @@
 function solution(id_pw, db) {
-    let order = [-1,-1];
-    let answer = ''
-    
-    //프로퍼티를 각각 비교 
-    for(let i=0;i<db.length;i++){
-        if(id_pw[0] == db[i][0]){
-            order[0] = i
-        }
-        
-        if(id_pw[1] == db[i][1]){
-            order[1] = i
-        }
+    for(let [id,pw] of db){
+        if(id_pw[0] == id) 
+            return id_pw[1] == pw ? "login" : "wrong pw"
     }
-
-    //order 중 string 찾기 / 숫자 null로 만들기 
-    console.log(order)
-    if(order[0] == order[1]){
-        answer = "login"
-    }else if(order[0] == -1){
-        answer = "fail"
-    }else{
-        answer = "wrong pw"
-    }
-
-    return answer;
+    return "fail";
 }
