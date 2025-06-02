9abfbe3f71164d052300a845e70dd5876b0f20cd
chaeyoung <happy7yong83@gmail.com>
2025-06-02 14:19:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 63.87 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 71.7 MB, 시간: 67.30 ms
+메모리: 70.9 MB, 시간: 63.87 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 26일 18:07:42
+2025년 06월 02일 14:19:13
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,15 +1,19 @@
 function solution(participant, completion) {
-    var map = {}; //해쉬
+    var answer = '';
+    let map ={};
     
-    for (let player of participant){
-        map[player] = (map[player] || 0) + 1
+    for(let people of participant){ //of는 배열 //in map 객체 키순회
+        map[people] = (map[people] || 0)+1 
     }
-    
+        
     for(let player of completion){
         map[player]--
     }
     
-    for(let player in map){
-        if(map[player] > 0) return player;
+    for(let player in map ){
+        if(map[player] > 0)
+            return player
     }
+    
+    return answer;
 }
