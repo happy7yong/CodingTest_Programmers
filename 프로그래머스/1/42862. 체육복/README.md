b6e1abb57989541d022b88097669f191f116122c
chaeyoung <happy7yong83@gmail.com>
2025-06-06 16:08:50 +0900

    [level 1] Title: 체육복, Time: 5.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 4.03 ms
+메모리: 33.6 MB, 시간: 5.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 05일 17:13:24
+2025년 06월 06일 16:08:48
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,21 +1,18 @@
 function solution(n, lost, reserve) {
     var answer = 0;
     lost.sort((a,b)=>a-b);
-    reserve.sort((a,b)=>a-b)
+    reserve.sort((a,b)=>a-b);
     
-    //1. 먼저 reserve와 lost의 번호가 겹치는게 있는지 확인 
-    let reserves = reserve.filter(student => !lost.includes(student))
-    let losts = lost.filter(student => !reserve.includes(student))
+    //앞이나 뒤만 빌려줄수있음. => 값이 하나밖에 차이나지 않는 학생들의 번호만 서로 빌려줄수있음.
+    //reserve가 있지만 lost한 학생들은 결국 하나밖에없음. => 중복학생들은 지움
+    let losts = lost.filter(_=>!reserve.includes(_));
+    let reserves = reserve.filter(_=>!lost.includes(_));
     
     for(let r of reserves){
-        let idx = losts.findIndex(l => Math.abs(r-l) === 1) //1단계밖에 차이가 안나는 idx / 잃어버린 학생을 기준으로, 빌려줄수있는 애가 있나확인
-        if(idx!=-1)losts.splice(idx,1) // idx에서 1개 삭제
+        let idx = losts.findIndex(i=>Math.abs(i-r)==1)
+        if (idx!=-1)losts.splice(idx, 1)//자르기를 splice()
     }
     
-    
-    
-    console.log(losts, reserves)
-    
-    //못빌린 사람만 빼면됨.
+    console.log(losts,reserves);
     return n-losts.length;
 }
