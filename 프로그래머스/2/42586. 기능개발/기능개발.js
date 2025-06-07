258f98c9306fa4f3341417f3218a23dd37820aec
chaeyoung <happy7yong83@gmail.com>
2025-06-07 17:14:55 +0900

    [level 2] Title: 기능개발, Time: 0.17 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,39 +1,20 @@
 function solution(progresses, speeds) {
-    let returnDay = [];             
-    //순서대로 작업이 진행되어야함.
-    //day로 시간 조절
-    //남은 일수 배열에 남은 일수를 저장하고, 다음 배열의 값이 앞의 프로세스 값보다 작을때, 함께 측정
+    var answer = [];
+    let leftDay = progresses.map((_,i)=> Math.ceil((100-_)/speeds[i]));
     
-    let leftDay = []; //남은 일수
-    
-    //progresses남은퍼센트 계산
-    for (let p of progresses){
-        let leftPro = 100-p;
-        leftDay.push(leftPro)
-    }
-    
-    //남은 일수
-    for (let i=0;i<speeds.length;i++){
-        leftDay[i] = Math.ceil(leftDay[i]/speeds[i])  
-    }
-    console.log(leftDay);
-    let TotalDay = leftDay.length;
-    
-    for(let curPro=0;curPro<TotalDay;curPro++){
-        let NextPro = 1;                                               
-        for(NextPro=1;NextPro<=TotalDay-(curPro+1);NextPro++){
-            if(leftDay[curPro+NextPro]<=leftDay[curPro]){
-                
-            }else if(leftDay[curPro+NextPro]>leftDay[curPro]){
-                break;
-            }
+    let i = 0;
+    while(i<progresses.length){
+        
+        let sucPro = 1; //현재 process는 무조건 배열 안에 들어가니 1
+        
+        //progresses[i]의 값이 다음값보다 클때 변수에 값 저장
+        while(leftDay[i] >= leftDay[i+sucPro]){
+            sucPro++;
         }
-        curPro+=NextPro-1;
-        returnDay.push(NextPro);
+        
+        answer.push(sucPro)
+        i+=sucPro //완료된 프로세스 건너뛴 후 인덱스
+        
     }
-    
-    
-    
-    
-    return returnDay;
+    return answer;
 }
