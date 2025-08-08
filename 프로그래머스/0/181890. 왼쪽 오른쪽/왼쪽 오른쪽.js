8ee159b2c87e5b45ce06f9b439b4a0375efbef70
chaeyoung <happy7yong83@gmail.com>
2025-08-08 16:44:05 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -0,0 +1,14 @@
+function solution(str_list) {
+    const l = str_list.indexOf("l");
+    const r = str_list.indexOf("r");
+
+    if (l !== -1 && (r === -1 || l < r)) {
+        return str_list.slice(0, l);
+    }
+
+    if (r !== -1 && (l === -1 || r < l)) {
+        return str_list.slice(r + 1);
+    }
+
+    return [];
+}
