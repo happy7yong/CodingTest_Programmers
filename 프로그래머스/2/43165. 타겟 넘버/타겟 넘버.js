5b12f37578655f51cf36a89ebfec95875499142f
chaeyoung <happy7yong83@gmail.com>
2025-12-09 14:02:02 +0900

    [level 2] Title: 타겟 넘버, Time: 42.10 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,26 +1,19 @@
 //DFS 백트래킹
 
-function dfs(index, numbers, sum, target, count){
-    //1. 종료 조건 = 트리의 끝
-    if(index === numbers.length){
-        //리프노드
-        return target == sum ? 1 :0 
-    }
-        
-    //2. 분기점 
-    const minus = dfs(index+1, numbers, sum - numbers[index], target, count)
-    const plus = dfs(index+1, numbers, sum + numbers[index], target, count)
+function dfs(start, arr, visited = new Set()){
     
-    return minus + plus
+    visited.add(start);
+    
+    for(const next of arr[start]){
+        
+    }
     
 } 
 
 function solution(numbers, target) {
-    //target : 되어야하는 값
-    //numbers : 그래프
-    
-    let sum = 0;
-    let index = 0;
-    let count = 0;
-    return dfs(index, numbers, sum, target, count);
+    var answer = numbers[0];
+
+
+    dfs(answer,numbers);
+    return answer;
 }
