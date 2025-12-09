ea243052bd210174bd93ed0f2dda662a7ce79580
chaeyoung <happy7yong83@gmail.com>
2025-12-09 11:33:57 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,35 +1,33 @@
 function solution(array) {
-    var counter = {};
+    let max = 0;
+    let maxCount = 0;
+    let maxKey = 0;
     
-    // 배열의 요소를 세는 부분
-    for (var i = 0; i < array.length; i++) {
-        var arr = array[i];
-        
-        if (counter[arr]) {
-            counter[arr] += 1;
-        } else {
-            counter[arr] = 1;
+    const map = {};
+    
+    //최빈값 찾기
+    //arr의 값을 키로 설정
+    for(const arr of array){
+        if(map[arr]) {
+            map[arr] += 1
+        }else{
+            map[arr] = 1
         }
     }
     
-    var maxCount = 0;
-    var maxElements = [];
+    
+    //가장 값이 큰 키찾기
+    for(const m in map){
+        max = max < map[m] ? map[m] : max
+    }
 
-    // 최빈값을 찾는 부분
-    for (var key in counter) {
-        if (counter[key] > maxCount) {
-            maxCount = counter[key];
-            maxElements = [key]; // 새로운 최빈값을 찾으면 배열 초기화
-        } else if (counter[key] === maxCount) {
-            maxElements.push(key); // 같은 최빈값이 있으면 추가
+    
+    for(const m in map){
+        if(max === map[m]){
+            maxCount ++;
+            maxKey = m;
         }
     }
     
-    // 최빈값이 여러 개일 경우 -1 반환
-    if (maxElements.length > 1) {
-        return -1;
-    }
-    
-    // 최빈값 반환
-    return parseInt(maxElements[0]); // 최빈값을 정수로 반환
+    return maxCount > 1 ? -1 : parseInt(maxKey);
 }
