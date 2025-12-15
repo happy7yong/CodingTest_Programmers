94dcfdffb22e6b49fa3443f3cae64313c1a07005
chaeyoung <happy7yong83@gmail.com>
2025-12-15 09:14:36 +0900

    [level 2] Title: 타겟 넘버, Time: 42.44 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,17 @@
-//DFS 백트래킹
-
-function dfs(start, arr, visited = new Set()){
-    
-    visited.add(start);
-    
-    for(const next of arr[start]){
-        
+function number(depth,numbers, target, num,count){
+    if(numbers.length === depth){
+       //리프노드 : 리프노드를 루프까지 어떻게 가져올건지 
+        if (num == target){ return 1;}else{return 0;}
     }
     
-} 
+    let minus = number(depth+1,numbers,target,num-numbers[depth],count);
+    let plus = number(depth+1,numbers,target,num+numbers[depth],count)
+    
+    return minus + plus;
+}
+
 
 function solution(numbers, target) {
-    var answer = numbers[0];
-
-
-    dfs(answer,numbers);
-    return answer;
+    let depth=0;
+    return number(depth,numbers, target,0,0);
 }
