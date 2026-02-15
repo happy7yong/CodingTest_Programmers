6e7960989a58282e56ef38dcb5adfdec0c165689
chaeyoung <happy7yong83@gmail.com>
2026-02-15 23:33:30 +0900

    [Bronze V] Title: A+B - 5, Time: 84 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").trim().split("\n");
+
+function solution(a,b){
+    return a+b;
+}
+
+let answer = [];
+for(let i =0;i<input.length;i++){
+    let [a,b] = input[i].split(" ").map(Number);
+    let s = solution(a,b);
+    if(s) answer.push(solution(a,b));
+}
+
+answer = answer.join("\n");
+console.log(answer);
