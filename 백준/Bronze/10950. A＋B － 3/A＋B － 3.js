9fb5ac6c6846cb3b7a19b3b2e35ec31b229713ec
chaeyoung <happy7yong83@gmail.com>
2026-02-15 23:24:07 +0900

    [Bronze V] Title: A+B - 3, Time: 92 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").trim().split("\n");
+
+function solution(a,b){
+    return a+b;
+}
+
+let answer = [];
+for(let i =1;i<input.length;i++){
+    let [a,b] = input[i].split(" ").map(Number);
+    answer.push(solution(a,b));
+}
+
+answer = answer.join("\n");
+console.log(answer);
