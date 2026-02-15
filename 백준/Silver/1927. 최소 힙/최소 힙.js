1c1f8c80bb3cd7ddbc02b83160bafa2e1cd55d7e
chaeyoung <happy7yong83@gmail.com>
2026-02-15 20:35:07 +0900

    [Silver II] Title: 최소 힙, Time: 212 ms, Memory: 28152 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.js
@@ -0,0 +1,94 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").trim().split("\n");
+
+class MinHeap{
+    constructor(){
+        this.heap = [];
+    }
+
+    insert(value){
+        let heap = this.heap;
+        heap.push(value);
+        this.heapifyUp();
+    }
+    extractMin(){
+        let heap = this.heap;
+        if(heap.length === 0) return null;
+        if(heap.length === 1) return heap.pop();
+
+        let min = heap[0];
+        heap[0] = heap.pop();
+
+        this.heapifyDown();
+        return min; 
+        
+    }
+    heapifyUp(){
+        let heap = this.heap;
+        let currentIndex = heap.length - 1;
+
+        while(currentIndex > 0){
+            let parentIndex = Math.floor((currentIndex - 1)/2);
+
+            if(heap[parentIndex] <= heap[currentIndex]) break;
+
+            let swap = heap[parentIndex];
+            heap[parentIndex] = heap[currentIndex];
+            heap[currentIndex] = swap;
+
+            currentIndex = parentIndex;
+        }
+    }
+
+    heapifyDown(){
+        let heap = this.heap;
+        let currentIndex = 0;
+
+        while(true) {
+            let leftChildIndex = currentIndex*2 + 1;
+            let rightChildIndex = currentIndex*2 + 2;
+            let minIndex = currentIndex;
+
+            if(leftChildIndex < heap.length && heap[leftChildIndex] < heap[minIndex]) minIndex = leftChildIndex;
+            if(rightChildIndex < heap.length && heap[rightChildIndex] < heap[minIndex]) minIndex = rightChildIndex;
+
+            if(minIndex === currentIndex) break;
+
+            let swap = heap[minIndex];
+            heap[minIndex] = heap[currentIndex];
+            heap[currentIndex] = swap;
+
+            currentIndex = minIndex;
+        }
+
+
+    }
+
+}
+
+function solution(n, arr){
+    let min = 0;
+    const minHeap = new MinHeap();
+    let answer = [];
+
+    for(let i of arr){
+        if(i === 0){
+            if(minHeap.heap.length === 0) {
+                answer.push(0);
+                continue;
+            }
+            min = minHeap.extractMin();
+            answer.push(min);
+            continue;
+        }
+        minHeap.insert(i)
+    }
+
+
+    return answer;
+}
+
+const n = Number(input[0]);
+const arr = input.slice(1).map(Number);
+
+console.log(solution(n, arr).join("\n"));
