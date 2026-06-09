10009169aef3dba2f7e60e70498da5bae378f11f
chaeyoung <happy7yong83@gmail.com>
2026-06-09 19:59:08 +0900

    [Passed] 알파벳에 따른 평가 (49ms, 16MB)

---
+++ codetree/trail1/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+n = input()
+
+if "S" == n :
+    print("Superior")
+elif "A" == n:
+    print("Excellent")
+elif "B" == n :
+    print("Good");
+elif "C" == n :
+    print("Usually")
+elif "D" == n :
+    print("Effort")
+else :
+    print("Failure")
