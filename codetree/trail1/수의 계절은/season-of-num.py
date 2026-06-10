db824933ca84bba8855485099e0d4e04bb62c32a
chaeyoung <happy7yong83@gmail.com>
2026-06-10 19:07:23 +0900

    [Passed] 수의 계절은 (48ms, 16MB)

---
+++ codetree/trail1/수의 계절은/season-of-num.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if a >= 12 :
+    print("Winter")
+elif a>= 9 :
+    print("Fall")
+elif a >= 6 :
+    print("Summer")
+elif a >= 3 :
+    print("Spring")
+else :
+    print("Winter")
