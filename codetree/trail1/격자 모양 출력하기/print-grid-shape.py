f402a6b70cc09dc1996192afd3cd06408975fcbd
chaeyoung <happy7yong83@gmail.com>
2026-06-19 15:13:38 +0900

    [Passed] 격자 모양 출력하기 (54ms, 16MB)

---
+++ codetree/trail1/격자 모양 출력하기/print-grid-shape.py
@@ -0,0 +1,9 @@
+n,m = map(int,input().split())
+
+arr = [[0 for i in range(n)] for _ in range(n)]
+
+for i in range(m) :
+    r,c = map(int,input().split())
+    arr[r-1][c-1] = r*c
+
+print("\n".join(" ".join(map(str,row)) for row in arr))
