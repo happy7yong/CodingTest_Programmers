bfb961ab6bc260c854e79596804a6d7e3bf8d53c
chaeyoung <happy7yong83@gmail.com>
2026-06-25 14:52:35 +0900

    [Passed] Next Level (47ms, 16MB)

---
+++ codetree/trail2/Next Level/next-level.py
@@ -0,0 +1,16 @@
+user2_id, user2_level = input().split()
+user2_level = int(user2_level)
+
+
+class User :
+    def __init__(self,id,level) :
+        self.id = id
+        self.level = level
+
+user1 = User("codetree", 10)
+user2 = User(user2_id, user2_level)
+
+print("user",user1.id,"lv",user1.level )
+print("user",user2.id,"lv",user2.level)
+
+    
