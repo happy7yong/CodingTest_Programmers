ba09f9ce3cd223de2d2d21053c51620b918bdbf0
chaeyoung <happy7yong83@gmail.com>
2026-06-26 15:40:17 +0900

    [Passed] 국영수 순이지 (56ms, 16MB)

---
+++ codetree/trail2/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,23 @@
+n = int(input())
+students = []
+
+class Student :
+    def __init__(self,name,kor,eng,math) :
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+for _ in range(n):
+    name,kor,eng,math = input().split()
+    kor,eng,math = int(kor),int(eng),int(math)
+    students.append(Student(name,kor,eng,math))
+
+students.sort(key=lambda s:(-s.kor,-s.eng,-s.math))
+
+for student in students :
+    print(student.name,student.kor,student.eng,student.math)
+
+
+    
+
