8a626c7f48000a40e3061cafb7cb14bb71096dce
chaeyoung <happy7yong83@gmail.com>
2026-07-06 18:32:22 +0900

    [Passed] 만나는 그 순간 (113ms, 34MB)

--- codetree/trail2/만나는 그 순간/the-moment-we-meet.py
+++ codetree/trail2/만나는 그 순간/the-moment-we-meet.py
@@ -1,46 +1,26 @@
 n, m = map(int, input().split())
+MAX_T = 1000000
 
-# def move(time,arr) :
-#     cnt = arr[-1]
-#     for i in range(time) :
-#         cnt+=1
-#         arrA.append(cnt)
-
-arrA = []
-cnt=0
+arrA,arrB = [0] * (MAX_T + 1), [0] * (MAX_T+1)
+time_a = 1
 for _ in range(n):
     direction, time = input().split()
-    time = int(time)
-
-    if direction == "R" :
-        for i in range(time) :
-            cnt+=1
-            arrA.append(cnt)
-    if direction == "L" :
-        for i in range(time) :
-            cnt-=1
-            arrA.append(cnt)
+    for _ in range(int(time)) :
+        arrA[time_a] = arrA[time_a-1] + (1 if direction == "R" else -1)
+        time_a += 1
 
 
-arrB = []
-cnt=0
+time_b = 1
 for _ in range(m):
     direction, time = input().split()
-    time = int(time)
-
-    if direction == "R" :
-        for i in range(time) :
-            cnt+=1
-            arrB.append(cnt)
-    if direction == "L" :
-        for i in range(time) :
-            cnt-=1
-            arrB.append(cnt)
+    for _ in range(int(time)) :
+        arrB[time_b] = arrB[time_b-1] + (1 if direction == "R" else -1)
+        time_b +=1
 
 ans = -1
-for i in range(len(arrA)) :
+for i in range(1,time_a) :
     if arrA[i] == arrB[i] :
-        ans = i+1
+        ans = i
         break
 
 print(ans)
